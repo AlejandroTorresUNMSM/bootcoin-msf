dda0b1f0268487c6ef1c862e93a51b33ccc0e741
AlejandroTorresUNSMS <alephdmc2575@gmail.com>
2023-08-01 12:42:39 -0500

    dev: commit inicial

---
+++ src/main/java/com/atorres/bootcoinmsf/BootcoinMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.bootcoinmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcoinMsfApplication.class, args);
+	}
+
+}
